3fcb91eb69c0f4e4fefb9896f17f489f684d31eb
Luis Cordova <91169088+LuchitoCordova@users.noreply.github.com>
2022-02-23 01:35:00 -0300

    Division del proyecto en Capas e Inicio de Capa Negocio

---
+++ UI/CapaDTO/Profesional.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDTO
+{
+    class Profesional
+    {
+        private int id;
+        private string rut;
+        private string nombre;
+        private string aPaterno;
+        private string aMaterno;
+        private string correo;
+        private string telefono;
+        private string contrasenna;
+
+        public int Id { get => id; set => id = value; }
+        public string Rut { get => rut; set => rut = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string APaterno { get => aPaterno; set => aPaterno = value; }
+        public string AMaterno { get => aMaterno; set => aMaterno = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+        public string Contrasenna { get => contrasenna; set => contrasenna = value; }
+
+    }
+}
---
+++ UI/CapaNegocio/Negocio.cs
@@ -0,0 +1,34 @@
+﻿using CapaConexion;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    class Negocio
+    {
+        private ConexionSQL conec;
+
+        public ConexionSQL Conec { get => conec; set => conec = value; }
+
+        public void configConex()
+        {
+            this.Conec = new ConexionSQL();
+            this.Conec.NombreTabla = "Usuario";
+            this.Conec.NombreBaseDatos = "portafolio";
+            this.Conec.CadenaConexion = @"Data Source=DESKTOP-398JQJ0\SQLEXPRESS;Initial Catalog=portafolio ;Integrated Security=True"; ;
+        }
+
+        public DataSet Login(string correo, string cont)
+        {
+            this.configConex();
+            this.Conec.CadenaSQL = "SELECT * FROM PROFESIONAL WHERE PROFESIONAL.Correo = '" + correo + "' AND PROFESIONAL.contrasenna = '" + cont + "';";
+            this.Conec.EsSelect = true;
+            this.Conec.conectar();
+            return this.Conec.DbDataSet;
+        }
+    }
+}
